0f03189e5f94b2a67f4787022d214d8b74b2a79e
Zhav1 <qhanakin@gmail.com>
2025-11-01 21:02:38 +0700

    backend for items

---
+++ laravel/app/Models/Item.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'status',
+        'lat',
+        'lng',
+        'cover_image_path',
+    ];
+
+    /**
+     * Get the user that owns the item.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
